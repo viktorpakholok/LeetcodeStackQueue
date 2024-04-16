68c153c6db676189fef6ee91e08a5d4157eade77
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-04-16 21:49:18 +0300

    Change Node init and added str method for "maximum_frequency_stack.py"

--- Maximum_Frequency_Stack/maximum_frequency_stack.py
+++ Maximum_Frequency_Stack/maximum_frequency_stack.py
@@ -2,9 +2,19 @@
 
 class Node:
     """"""
-    def __init__(self, data, next_=None):
+    def __init__(self, data, amount = 1, next_ = None):
         self.data = data
+        self.amount = amount
         self.next = next_
+
+    def __str__(self) -> str:
+        res = 'Node: '
+        head = self
+        while head:
+            res += f'({head.data}, {head.amount}) -> '
+            head = head.next
+        return res + 'None'
+
 
 class FreqStack:
     """"""
