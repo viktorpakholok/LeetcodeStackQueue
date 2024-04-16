16f767f0b2ba9fea3b83c7310fe2822587a680c2
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-04-16 21:53:20 +0300

    Added init and push for FreqStack in "maximum_frequency_stack.py"

--- Maximum_Frequency_Stack/maximum_frequency_stack.py
+++ Maximum_Frequency_Stack/maximum_frequency_stack.py
@@ -18,3 +18,21 @@ class Node:
 
 class FreqStack:
     """"""
+    def __init__(self):
+        self.head = None
+        self.max_ = 1
+
+    def push(self, val: int) -> None:
+        if self.head is None:
+            self.head = Node(val, 1)
+        else:
+            head = self.head
+            to_rem = 1
+            while head:
+                if head.data == val:
+                    to_rem = head.amount + 1
+                    if to_rem > self.max_:
+                        self.max_ = to_rem
+                    break
+                head = head.next
+            self.head = Node(val, to_rem, self.head)
