d2c1e7c1dd48ca414957b5f619e3a8f1884d9868
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-04-16 21:46:24 +0300

    Added docstrings and typehinting to "implement_stack_using_queues.py"

--- Implement_Stack_using_Queues/implement_stack_using_queues.py
+++ Implement_Stack_using_Queues/implement_stack_using_queues.py
@@ -1,31 +1,31 @@
-""""""
+"""A module with realised Stack using two queues"""
 
 class Node:
-    """"""
-    def __init__(self, data, next_=None):
+    """A class representing a node in a linked list"""
+    def __init__(self, data: int, next_ = None):
         self.data = data
         self.next = next_
 
 class MyQueue:
-    """"""
+    """A class representing a queue data structure using linked lists"""
     def __init__(self):
         self.head = None
         self.tail = None
 
-    def push(self, val):
-        """"""
+    def push(self, val: int) -> None:
+        """Pushes an element in the queue"""
         if self.head is None:
             self.tail = Node(val)
             self.head = self.tail
         else:
             self.head = Node(val, self.head)
 
-    def peek(self):
-        """"""
+    def peek(self) -> int:
+        """Returns the value of the last element in the queue"""
         return self.tail
 
-    def pop(self):
-        """"""
+    def pop(self) -> int:
+        """Removes and returns the front element from the queue"""
         head = self.head
         if head.next is None:
             self.head = None
@@ -40,7 +40,7 @@ class MyQueue:
         head.next = None
         return was.data
 
-    def __str__(self):
+    def __str__(self) -> str:
         head = self.head
         res = 'Queue: '
         while head:
@@ -48,27 +48,29 @@ class MyQueue:
             head = head.next
         return res + 'None'
 
-    def __len__(self):
+    def __len__(self) -> int:
         count, head = 0, self.head
         while head:
             count += 1
             head = head.next
         return count
 
-    def is_empty(self):
-        """"""
+    def is_empty(self) -> bool:
+        """Checks if the queue is empty"""
         return self.head is None
 
 class MyStack:
-    """"""
+    """A class representing a stack data structure using two queues"""
     def __init__(self):
         self.one_q = MyQueue()
         self.two_q = MyQueue()
 
     def push(self, x: int) -> None:
+        """Pushes an element onto the stack"""
         self.one_q.push(x)
 
     def pop(self) -> int:
+        """Removes and returns the top element from the stack"""
         head = self.one_q.head
         while head.next:
             self.two_q.push(self.one_q.pop())
@@ -76,8 +78,8 @@ class MyStack:
         self.one_q, self.two_q = self.two_q, self.one_q
         return self.two_q.pop()
 
-
     def top(self) -> int:
+        """Returns the value of the top element without removing it"""
         head = self.one_q.head
         while head.next:
             self.two_q.push(self.one_q.pop())
@@ -89,4 +91,5 @@ class MyStack:
         return ret
 
     def empty(self) -> bool:
+        """Checks if the stack is empty"""
         return self.one_q.is_empty()
