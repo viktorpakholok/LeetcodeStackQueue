a90c49a329053033726c982046cac2b79c3a70c4
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-04-16 20:32:41 +0300

    Made a basic structure

---
+++ Implement_Queue_using_Stacks/implement_queue_using_stacks.py
@@ -0,0 +1,13 @@
+""""""
+
+class Node:
+    """"""
+    def __init__(self, data, next_=None):
+        self.data = data
+        self.next = next_
+
+class MyQueue:
+    """"""
+
+class MyStack:
+    """"""
---
+++ Maximum_Frequency_Stack/maximum_frequency_stack.py
@@ -0,0 +1,10 @@
+""""""
+
+class Node:
+    """"""
+    def __init__(self, data, next_=None):
+        self.data = data
+        self.next = next_
+
+class FreqStack:
+    """"""
