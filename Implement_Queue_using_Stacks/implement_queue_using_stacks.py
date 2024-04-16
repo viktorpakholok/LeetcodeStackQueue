d5919f9c72887d66723dd16c16d60864e0164047
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-04-16 21:11:21 +0300

    Added MyStack class to "implement_queue_using_stacks.py"

--- Implement_Queue_using_Stacks/implement_queue_using_stacks.py
+++ Implement_Queue_using_Stacks/implement_queue_using_stacks.py
@@ -6,8 +6,41 @@ class Node:
         self.data = data
         self.next = next_
 
-class MyQueue:
-    """"""
+    def __repr__(self) -> str:
+        return f'Node({self.data}, {self.next})'
+
 
 class MyStack:
     """"""
+    def __init__(self) -> None:
+        self.head = None
+
+    def push(self, val):
+        """"""
+        if self.head is None:
+            self.head = Node(val)
+        else:
+            self.head = Node(val, self.head)
+
+    def pop(self):
+        """"""
+        was = self.head.data
+        self.head = self.head.next
+        return was
+
+    def peek(self):
+        """"""
+        return self.head.data
+
+    def empty(self):
+        """"""
+        return self.head is None
+
+    def __str__(self) -> str:
+        """"""
+        head = self.head
+        res = 'Stack: '
+        while head:
+            res += str(head.data) + ' -> '
+            head = head.next
+        return res + 'None'
